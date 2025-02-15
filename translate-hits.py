a908682e33c9946654c9faacf31f6f3737567b77
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2025-02-15 13:06:39 +0100

    translations

---
+++ translate-hits.py
@@ -0,0 +1,58 @@
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import os
+import sqlite3
+
+def generate_translation(input: str, tokenizer: MarianTokenizer, model: MarianMTModel) -> str :
+    inputs = tokenizer(input, return_tensors="pt", padding=True, truncation=True)
+    translated = model.generate(**inputs)
+    return tokenizer.batch_decode(translated, skip_special_tokens=True)[0]
+
+def do_translations(data_dir: str, input_file: str, output_file: str):
+    if not os.path.exists(input_file):
+        raise ValueError(f"Input file not found: {input_file}")
+
+    tokenizer_nl = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-nl-en")
+    model_nl = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-nl-en")
+    model_nl.to("cuda")
+    tokenizer_fr = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-fr-en")
+    model_fr = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-fr-en")
+    model_fr.to("cuda")
+
+    with sqlite3.connect(input_file) as input_conn, sqlite3.connect(output_file) as output_conn:
+        output_conn.execute("drop table if exists hits_translation;")
+        output_conn.execute("create table if not exists hits_translation (url text, content text, content_en, languages text);")
+
+        result = input_conn.execute("select url, content, languages from hits limit 5;").fetchall()
+        urls = [r[0] for r in result]
+        documents = [r[1] for r in result]
+        languages = [r[2] for r in result]
+
+        for i, doc in enumerate(documents):
+            translation = None
+            if ("fra" in languages[i]):
+                print(f"document {i} is in french")
+                translation = generate_translation(doc, tokenizer_fr, model_translation_fr)
+            elif ("nld" in languages[i]):
+                print(f"document {i} is in dutch")
+                translation = generate_translation(doc, tokenizer_nl, model_translation_nl)
+            else:
+                print(f"document {i} will not be translated")
+                translation = doc
+            output_conn.execute("insert into hits_translation (url, content, content_en, languages) values (?, ?, ?, ?);", (urls[i], doc, translation, languages[i]))
+
+
+if __nam__ == __main__:
+    data_dir = os.environ["PROJECT_DATA_DIR"]
+    input_file = os.environ["INPUT_FILE"]
+    output_file = os.environ["OUTPUT_FILE"]
+
+    if (data_dir is None):
+        raise ValueError("Data directory not set")
+    if (input_file is None):
+        raise ValueError("Input file not set")
+    if (output_file is None):
+        raise ValueError("Output file not set")
+
+    do_translations(data_dir, input_file, output_file)
---
+++ translate-job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+## This file is called `translate-job.sh`
+#SBATCH --time=00:15:00
+#SBATCH --account=p200769
+#SBATCH --partition=gpu
+#SBATCH --qos=dev
+#SBATCH --nodes=1
+
+module load Python
+
+python -c  'import sys; print(sys.version)'
+
+python -m venv venv
+
+source venv/bin/activate
+
+python -m pip --cache-dir=/project/home/p200769/data/pip install -r requirements.txt
+
+export PROJECT_DATA_DIR=/project/home/p2000769/data
+export INPUT_FILE=filtered-hits-all-sqlite
+export OUTPUT_FILE=translated-filtered-hits-all.sqlite
+export TRANSFORMERS_CACHE = /project/home/p2000769/data/huggingface
+export HUGGINGFACE_HUB_CACHE = /project/home/p2000769/data/huggingface
+
+python translate-hits.py
