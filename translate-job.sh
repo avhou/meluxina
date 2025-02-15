bcccf8eb59c08fd9b1141a4f9af4e802ca1b3465
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2025-02-15 14:30:05 +0100

    enkel HF_HOME overhouden

--- translate-job.sh
+++ translate-job.sh
@@ -20,7 +20,6 @@ export PROJECT_DATA_DIR=/project/home/p200769/data
 export INPUT_FILE=filtered-hits-all.sqlite
 export OUTPUT_FILE=translated-filtered-hits-all.sqlite
 export HF_HOME=/project/home/p200769/data/huggingface
-export TRANSFORMERS_CACHE=/project/home/p200769/data/huggingface
 export HUGGINGFACE_HUB_CACHE=/project/home/p200769/data/huggingface
 
 python translate-hits.py
