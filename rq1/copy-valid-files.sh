fb8a576a8b2829e7b3eeb771690cedc9621faf83
Alexander Van Hecke <alexander@m1mac.local>
2025-03-29 10:40:03 +0100

    batch file

---
+++ rq1/copy-valid-files.sh
@@ -0,0 +1,3 @@
+cp rq1_deepseek-ai_DeepSeek-R1-Distill-Llama-70B_few-shot_valid.json rq1_deepseek-ai_DeepSeek-R1-Distill-Llama-70B_few-shot.json
+cp rq1_deepseek-ai_DeepSeek-R1-Distill-Llama-70B_zero-shot_valid.json rq1_deepseek-ai_DeepSeek-R1-Distill-Llama-70B_zero-shot.json
+cp rq1_deepseek-ai_DeepSeek-R1-Distill-Llama-70B_one-shot_valid.json rq1_deepseek-ai_DeepSeek-R1-Distill-Llama-70B_one-shot.json
