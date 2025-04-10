04e81cd80832c46092250a619c6c12051624d8a7
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2025-04-10 15:36:59 +0200

    meer output

--- rq2/rq2-rdf2vec-preparation.py
+++ rq2/rq2-rdf2vec-preparation.py
@@ -11,7 +11,10 @@ from models import Output
 
 class ArticleChunk(BaseModel):
     article_url: str
+    article_number: int
+    article_number_total: int
     chunk_number: int
+    chunk_number_total: int
     chunk_text: str
 
 
@@ -25,6 +28,10 @@ def process_chunks_of_articles(
     print(f"processing input database {input_database}")
     chunker = SentenceSplitter(chunk_size=max_words, chunk_overlap=overlap)
     with sqlite3.connect(input_database) as conn:
+        article_number_total = conn.execute(
+            "select count(url) from articles"
+        ).fetchone()[0]
+        article_number = 0
         for row in (
             conn.execute("select url, translated_text from articles")
             if use_translated_text
@@ -33,9 +40,20 @@ def process_chunks_of_articles(
             url = row[0]
             text = row[1]
             text = text.replace("\\s+", "")
-            for index, chunk in enumerate(chunker.split_text(text)):
+            article_number = article_number + 1
+
+            chunks = chunker.split_text(text)
+
+            for index, chunk in enumerate(chunks):
                 callback(
-                    ArticleChunk(article_url=url, chunk_number=index, chunk_text=chunk)
+                    ArticleChunk(
+                        article_url=url,
+                        article_number=article_number,
+                        article_number_total=article_number_total,
+                        chunk_number=index + 1,
+                        chunk_number_total=len(chunks),
+                        chunk_text=chunk,
+                    )
                 )
 
 
@@ -64,34 +82,57 @@ def rq2_rdf2vec_preparation(input_database: str, output_database: str):
         f"""Done loading model at {datetime.now().strftime("%H:%M:%S")}""", flush=True
     )
 
-    def process_chunk(chunk: ArticleChunk):
+    def process_chunk(chunk: ArticleChunk, conn):
         print(
-            f"processing chunk {chunk.chunk_number}, url {chunk.article_url}, should write to {output_database}"
+            f"{datetime.now().strftime('%H:%M:%S')}: start processing chunk {chunk.chunk_number}/{chunk.chunk_number_total}, article {chunk.article_number}/{chunk.article_number_total}, url {chunk.article_url}"
         )
 
-        messages = [
-            {
-                "role": "system",
-                "content": f"""You are an expert AI system that specializes in named entity recognition and knowledge graph extraction. 
-            Your task is to analyse any provided input text, identify and extract the relevant entities and their attributes and relationships, 
-            and output a knowledge graph.  
-            You will output triples (subject, predicate, object) in JSON format.   
-            The output should conform to this JSON schema : {Output.model_json_schema()}.  
-            Only output the JSON-formatted knowledge graph and do not include any explanations.  
-            Be as succinct as possible and only include the most relevant information.
-            However, make sure to list just one concept per subject, predicate or object.  If a subject, predicate or object contains multiple concepts, split them into separate triples.
-            """,
-            },
-            {"role": "user", "content": chunk.chunk_text},
-        ]
+        bestaat = conn.execute(
+            f"select exists(select 1 from chunked_articles where url={chunk.article_url} and chunk_number={chunk.chunk_number})"
+        ).fetchone()[0]
 
-        outputs = pipeline(messages, max_new_tokens=1024)
-        json = outputs[0]["generated_text"][-1]["content"]
-        print(
-            f"chunk {chunk.chunk_number}, url {chunk.article_url} generated output {json}"
+        if bestaat == 1:
+            print(
+                f"{datetime.now().strftime('%H:%M:%S')}: skip chunk {chunk.chunk_number}/{chunk.chunk_number_total}, article {chunk.article_number}/{chunk.article_number_total}, url {chunk.article_url}, already done"
+            )
+        else:
+            messages = [
+                {
+                    "role": "system",
+                    "content": f"""You are an expert AI system that specializes in named entity recognition and knowledge graph extraction. 
+                Your task is to analyse any provided input text, identify and extract the relevant entities and their attributes and relationships, 
+                and output a knowledge graph.  
+                You will output triples (subject, predicate, object) in JSON format.   
+                The output should conform to this JSON schema : {Output.model_json_schema()}.  
+                Only output the JSON-formatted knowledge graph and do not include any explanations.  
+                Be as succinct as possible and only include the most relevant information, while maintaining coherence in the knowledge graph.
+                Make sure to list just one concept per subject, predicate or object.  If a subject, predicate or object contains multiple concepts, split them into separate triples.
+                """,
+                },
+                {"role": "user", "content": chunk.chunk_text},
+            ]
+
+            outputs = pipeline(messages, max_new_tokens=1024)
+            json = outputs[0]["generated_text"][-1]["content"]
+            json = f"{json}".replace("\\s+", " ")
+            conn.execute(
+                "insert into chunked_articles(url, chunk_number, chunk_text, chunk_triples) values (?, ?, ?, ?)",
+                (chunk.article_url, chunk.chunk_number, chunk.chunk_text, json),
+            )
+            print(
+                f"{datetime.now().strftime('%H:%M:%S')}: {chunk.chunk_number}/{chunk.chunk_number_total}, article {chunk.article_number}/{chunk.article_number_total}, url {chunk.article_url} generated output {json}"
+            )
+
+    with sqlite3.connect("output_database") as conn:
+        conn.execute(
+            "create table if not exists chunked_articles(url text, chunk_number int, chunk_text text, chunk_triples text)"
         )
-
-    process_chunks_of_articles(input_database, process_chunk)
+        process_chunks_of_articles(
+            input_database,
+            lambda chunk: process_chunk(chunk, conn),
+            max_words=10,
+            overlap=0,
+        )
 
 
 if __name__ == "__main__":
