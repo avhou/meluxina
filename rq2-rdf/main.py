27aed2d8473fd1fb5337a9d3144e058be04f17f4
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2025-04-12 10:06:09 +0200

    eerste versie met hardcoded rdf graph

--- rq2-rdf/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from rq2-rdf!")
-
-
-if __name__ == "__main__":
-    main()
--- rq2-rdf/rq2-rdf2vec.py
+++ rq2-rdf/rq2-rdf2vec.py
@@ -1,33 +1,40 @@
-from rdf2vec.graphs import KnowledgeGraph
-from rdf2vec.walkers import RandomWalker
-from rdf2vec.embedders import Word2Vec
-from rdf2vec.rdf2vec import RDF2VecTransformer
-from rdflib import URIRef, Literal
+from pyrdf2vec.walkers import RandomWalker
+from pyrdf2vec.embedders import Word2Vec
+from pyrdf2vec.rdf2vec import RDF2VecTransformer
+from rdflib import Literal
+from pyrdf2vec.samplers import PageRankSampler
+
+
+from pyrdf2vec.graphs import KG, Vertex
 
 triples = [
-    (URIRef("http://example.org/Person/Alice"), URIRef("http://example.org/knows"), URIRef("http://example.org/Person/Bob")),
-    (URIRef("http://example.org/Person/Alice"), URIRef("http://example.org/role"), Literal("immigration activist")),
-    (URIRef("http://example.org/Person/Bob"), URIRef("http://example.org/associatedWith"), URIRef("http://example.org/Org/DisinfoInc")),
+    [Literal("Alice"), Literal("knows"), Literal("Bob")],
+    [Literal("Alice"), Literal("knows"), Literal("Dean")],
+    [Literal("Dean"), Literal("loves"), Literal("Alice")],
 ]
+# url = "http://pyRDF2Vec"
+kg = KG()
 
-kg = KnowledgeGraph()
-kg._triples.clear()  # clear default content
+for row in triples:
+    subj = Vertex(str(row[0]))
+    obj = Vertex(str(row[2]))
+    pred = Vertex(str(row[1]), predicate=True, vprev=subj, vnext=obj)
+    kg.add_walk(subj, pred, obj)
 
-for s, p, o in triples:
-    kg.add_triple(s, p, o)
 
-# Prepare entities
-# entities = list({s for s, _, _ in triples})
-entities = list(kg._triples.keys())
+unique_subjects = list({str(s) for s, _, _ in triples})
+unique_objects = list({str(o) for _, _, o in triples})
+unique_subjects_and_objects = list(set(unique_subjects + unique_objects))
+# we willen in principe kunnen queryen op de subjects
+entities = unique_subjects
 
-# Setup and train RDF2Vec
-walker = RandomWalker(depth=2, walks_per_entity=5)
-embedder = Word2Vec(vector_size=50)
-transformer = RDF2VecTransformer(walkers=[walker], embedder=embedder)
-
-# this is a dict[URI] -> vector
-embeddings = transformer.fit_transform(kg, entities)
-
-# Map embeddings back to entities
-for uri, vector in zip(entities, embeddings):
-    print(f"{uri} → {vector[:5]}...")  # Preview vector
+# # Setup and train RDF2Vec
+walker = RandomWalker(4, 10, PageRankSampler())
+embedder = Word2Vec(epochs=10)
+transformer = RDF2VecTransformer(embedder, walkers=[walker], verbose=1)
+#
+# embeddings is een lijst van vectors, de size van de gevraagde entiteiten.  dus elke entiteit heeft een embedding vector
+embeddings, _ = transformer.fit_transform(kg, entities)
+#
+print("embeddings : ")
+print(len(embeddings))
