714de2eaabbc66faae7cc14a7421eaf26de8e8a8
Alexander Van Hecke <alexander.vanhecke@student.ou.nl>
2025-04-12 13:27:59 +0200

    cleanup

--- rq2-rdf/rq2-rdf2vec.py
+++ rq2-rdf/rq2-rdf2vec.py
@@ -1,59 +1,13 @@
 from pyrdf2vec.walkers import RandomWalker
 from pyrdf2vec.embedders import Word2Vec
 from pyrdf2vec.rdf2vec import RDF2VecTransformer
-from rdflib import Literal
 from pyrdf2vec.samplers import PageRankSampler
 import sys
 import sqlite3
 from models import Output, Triple
 from typing import List
 from unidecode import unidecode
-
-
 from pyrdf2vec.graphs import KG, Vertex
-
-triples = [
-    ["Alice", "knows", "Bob"],
-    ["Alice", "knows", "Dean"],
-    ["Dean", "loves", "Alice"],
-]
-
-kg = KG()
-
-for s, p, o in triples:
-    subj = Vertex(s)
-    obj = Vertex(o)
-    pred = Vertex(p, predicate=True, vprev=subj, vnext=obj)
-    kg.add_walk(subj, pred, obj)
-
-
-unique_subjects = list({s for s, _, _ in triples})
-unique_objects = list({o for _, _, o in triples})
-unique_subjects_and_objects = list(set(unique_subjects + unique_objects))
-# we willen in principe kunnen queryen op de subjects
-entities = unique_subjects
-
-# # Setup and train RDF2Vec
-# use a random walker with a maximum of 10 walks of a maximum depth of 4
-# walker = RandomWalker(4, 10, PageRankSampler(), random_state=1234)
-walker = RandomWalker(4, None, PageRankSampler(), random_state=1234)
-embedder = Word2Vec(epochs=10, workers=1)
-transformer = RDF2VecTransformer(embedder, walkers=[walker], verbose=1)
-#
-# embeddings is een lijst van vectors, de size van de gevraagde entiteiten.  dus elke entiteit heeft een embedding vector
-embeddings, _ = transformer.fit_transform(kg, entities)
-#
-print("embeddings : ")
-print(len(embeddings))
-
-for entity, embedding in zip(entities, embeddings):
-    print(f"entiteit {entity} heeft embedding {embedding[:4]}")
-
-# doc_entities = ["Tom"]
-#
-# doc_embeddings, _ = transformer.transform(kg, doc_entities)
-#
-# print(doc_embeddings)
 
 
 def normalize(value: str) -> str:
